851dec62debc8e2cf6f56e816a2d50b5f97b9a3b
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3093

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def countSubMultisets(self, nums: List[int], l: int, r: int) -> int:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.countSubMultisets([1, 2, 2, 3], 6, 6) == 1
-assert obj.countSubMultisets([2, 1, 4, 2, 7], 1, 5) == 7
-assert obj.countSubMultisets([1, 2, 1, 3, 5, 2], 3, 5) == 9
+assert obj.sumIndicesWithKSetBits([5, 10, 1, 5, 2], 1) == 13
+assert obj.sumIndicesWithKSetBits([4, 3, 2, 1], 2) == 1
