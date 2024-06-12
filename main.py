35aec6bb2374159f0353915fbc4d56d31ae947d7
swe-bench <>
2024-06-12 16:11:49 -0400

    Task instance 2779

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+    def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
         pass
 
 obj = Solution()
-assert obj.distinctDifferenceArray([1, 2, 3, 4, 5]) == [-3, -1, 1, 3, 5]
-assert obj.distinctDifferenceArray([3, 2, 3, 4, 2]) == [-2, -1, 0, 2, 3]
+assert obj.colorTheArray(4, [[0, 2], [1, 2], [3, 1], [1, 1], [2, 1]]) == [0, 1, 1, 0, 2]
+assert obj.colorTheArray(1, [[0, 100000]]) == [0]
