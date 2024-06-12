d3125ca5487e2e236800c26e62dec92b65670490
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2921

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from typing import List
-
 class Solution:
-    def minimumSeconds(self, nums: List[int]) -> int:
+    def countSteppingNumbers(self, low: str, high: str) -> int:
         pass
 
 obj = Solution()
-assert obj.minimumSeconds([1, 2, 1, 2]) == 1
-assert obj.minimumSeconds([2, 1, 3, 3, 2]) == 2
-assert obj.minimumSeconds([5, 5, 5, 5]) == 0
+assert obj.countSteppingNumbers("1", "11") == 10
+assert obj.countSteppingNumbers("90", "101") == 2
