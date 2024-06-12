3e683216a736622abcfcb115816432a76042b2b4
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2891

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def minimumIndex(self, nums: List[int]) -> int:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minimumIndex([1, 2, 2, 2]) == 2
-assert obj.minimumIndex([2, 1, 3, 1, 1, 1, 7, 1, 2, 1]) == 4
-assert obj.minimumIndex([3, 3, 3, 3, 7, 2, 2]) == -1
+assert obj.maximumBeauty([4, 6, 1, 2], 2) == 3
+assert obj.maximumBeauty([1, 1, 1, 1], 10) == 4
