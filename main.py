8e071fbe676525c9df9177ded98ae78f26600391
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3164

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def sumCounts(self, nums: List[int]) -> int:
+    def lastVisitedIntegers(self, words: List[str]) -> List[int]:
         pass
 
 obj = Solution()
-assert obj.sumCounts([1, 2, 1]) == 15
-assert obj.sumCounts([1, 1]) == 3
+assert obj.lastVisitedIntegers(["1", "2", "prev", "prev", "prev"]) == [2, 1, -1]
+assert obj.lastVisitedIntegers(["1", "prev", "2", "prev", "prev"]) == [1, 2, 1]
