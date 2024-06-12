de09ae490b628a63d9457f56693ea660bb48e789
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3153

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+    def maxSum(self, nums: List[int], k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minProcessingTime([8, 10], [2, 2, 3, 1, 8, 7, 4, 5]) == 16
-assert obj.minProcessingTime([10, 20], [2, 3, 1, 2, 5, 8, 4, 3]) == 23
+assert obj.maxSum([2, 6, 5, 8], 2) == 261
+assert obj.maxSum([4, 5, 4, 7], 3) == 90
