7bb0dca0c3ac7281cfbc519961919b5000dbe429
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3032

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+    def getMaxFunctionValue(self, receiver: List[int], k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.constructProductMatrix([[1, 2], [3, 4]]) == [[24, 12], [8, 6]]
-assert obj.constructProductMatrix([[12345], [2], [1]]) == [[2], [0], [0]]
+assert obj.getMaxFunctionValue([2, 0, 1], 4) == 6
+assert obj.getMaxFunctionValue([1, 1, 1, 2, 3], 3) == 10
