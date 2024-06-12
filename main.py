387f16a091a203ce24dbc3289b6c6c531b944cb5
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3203

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import List
+
 class Solution:
-    def stringCount(self, n: int) -> int:
+    def canMakePalindromeQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
         pass
 
 obj = Solution()
-assert obj.stringCount(4) == 12
-assert obj.stringCount(10) == 83943898
+assert obj.canMakePalindromeQueries("abcabc", [[1, 1, 3, 5], [0, 2, 5, 5]]) == [True, True]
+assert obj.canMakePalindromeQueries("abbcdecbba", [[0, 2, 7, 9]]) == [False]
+assert obj.canMakePalindromeQueries("acbcab", [[1, 2, 4, 5]]) == [True]
