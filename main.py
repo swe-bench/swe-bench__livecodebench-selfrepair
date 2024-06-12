10cd0fc1cd646ca364141ec886be0e933cb6f005
swe-bench <>
2024-06-12 16:11:54 -0400

    Task instance 3229

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 class Solution:
-    def maximumSetSize(self, nums1: List[int], nums2: List[int]) -> int:
+    def minimumCost(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.maximumSetSize([1, 2, 1, 2], [1, 1, 1, 1]) == 2
-assert obj.maximumSetSize([1, 2, 3, 4, 5, 6], [2, 3, 2, 3, 2, 3]) == 5
-assert obj.maximumSetSize([1, 1, 2, 2, 3, 3], [4, 4, 5, 5, 6, 6]) == 6
+assert obj.minimumCost([1, 2, 3, 4, 5]) == 6
+assert obj.minimumCost([10, 12, 13, 14, 15]) == 11
+assert obj.minimumCost([22, 33, 22, 33, 22]) == 22
