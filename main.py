8537253a570b94748c330ec3743b3b4c936362ef
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3141

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 class Solution:
-    def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
         pass
 
 obj = Solution()
-assert obj.maximumSumOfHeights([5, 3, 4, 1, 1]) == 13
-assert obj.maximumSumOfHeights([6, 5, 3, 9, 2, 7]) == 22
-assert obj.maximumSumOfHeights([3, 2, 5, 5, 2, 3]) == 18
+assert obj.minSizeSubarray([1, 2, 3], 5) == 2
+assert obj.minSizeSubarray([1, 1, 1, 2, 3], 4) == 2
+assert obj.minSizeSubarray([2, 4, 6, 8], 3) == -1
