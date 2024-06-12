4a5d03b022389a46d8d9f5b69eca045e9f67d2c8
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 2998

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from typing import List
-
 class Solution:
-    def maximizeTheProfit(self, n: int, offers: List[List[int]]) -> int:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
         pass
 
 obj = Solution()
-assert obj.maximizeTheProfit(5, [[0, 0, 1], [0, 2, 2], [1, 3, 2]]) == 3
-assert obj.maximizeTheProfit(5, [[0, 0, 1], [0, 2, 10], [1, 3, 2]]) == 10
+assert obj.countSymmetricIntegers(1, 100) == 9
+assert obj.countSymmetricIntegers(1200, 1230) == 4
