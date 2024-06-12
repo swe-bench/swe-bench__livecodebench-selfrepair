885b49efdb8bb0535e9bfec732434fb09abea2c2
swe-bench <>
2024-06-12 16:11:49 -0400

    Task instance 2756

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+    def buyChoco(self, prices: List[int], money: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minExtraChar("leetscode", ["leet", "code", "leetcode"]) == 1
-assert obj.minExtraChar("sayhelloworld", ["hello", "world"]) == 3
+assert obj.buyChoco([1, 2, 2], 3) == 0
+assert obj.buyChoco([3, 2, 3], 3) == 3
