662a428c9d34153b523329c02f67b083b26e471a
swe-bench <>
2024-06-12 16:11:50 -0400

    Task instance 2847

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import List
 
 class Solution:
-    def findValueOfPartition(self, nums: List[int]) -> int:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
         pass
 
 obj = Solution()
-assert obj.findValueOfPartition([1, 3, 2, 4]) == 1
-assert obj.findValueOfPartition([100, 1, 10]) == 9
+assert obj.maximumNumberOfStringPairs(["cd", "ac", "dc", "ca", "zz"]) == 2
+assert obj.maximumNumberOfStringPairs(["ab", "ba", "cc"]) == 1
+assert obj.maximumNumberOfStringPairs(["aa", "ab"]) == 0
