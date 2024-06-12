0a3bd0fb425408d5314800bb75d0b725c01b9d6e
swe-bench <>
2024-06-12 16:11:54 -0400

    Task instance 3262

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 class Solution:
-    def minOrAfterOperations(self, nums: List[int], k: int) -> int:
+    def largestPerimeter(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.minOrAfterOperations([3, 5, 3, 2, 7], 2) == 3
-assert obj.minOrAfterOperations([7, 3, 15, 14, 2, 8], 4) == 2
-assert obj.minOrAfterOperations([10, 7, 10, 3, 9, 14, 9, 4], 1) == 15
+assert obj.largestPerimeter([5, 5, 5]) == 15
+assert obj.largestPerimeter([1, 12, 1, 2, 5, 50, 3]) == 12
+assert obj.largestPerimeter([5, 5, 50]) == -1
