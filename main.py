d887d072a66c13ebee612a50333823548a4aefab
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3044

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import List
 
 class Solution:
-    def numberOfPoints(self, nums: List[List[int]]) -> int:
+    def minOperations(self, nums: List[int], k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.numberOfPoints([[3, 6], [1, 5], [4, 7]]) == 7
-assert obj.numberOfPoints([[1, 3], [5, 8]]) == 7
+assert obj.minOperations([3, 1, 5, 4, 2], 2) == 4
+assert obj.minOperations([3, 1, 5, 4, 2], 5) == 5
+assert obj.minOperations([3, 2, 5, 3, 1], 3) == 4
