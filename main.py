6f6a1e67fe7aab78899c296a7d8da3690427e2c8
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2878

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import List
+
 class Solution:
-    def minimumString(self, a: str, b: str, c: str) -> str:
+    def checkArray(self, nums: List[int], k: int) -> bool:
         pass
 
 obj = Solution()
-assert obj.minimumString("abc", "bca", "aaa") == "aaabca"
-assert obj.minimumString("ab", "ba", "aba") == "aba"
+assert obj.checkArray([2, 2, 3, 1, 1, 0], 3) == True
+assert obj.checkArray([1, 3, 1, 1], 2) == False
