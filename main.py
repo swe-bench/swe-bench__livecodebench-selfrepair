2bc9285dc0493049a79a06d384e3cc073f8d4d25
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3190

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import List
 
 class Solution:
-    def findChampion(self, grid: List[List[int]]) -> int:
+    def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.findChampion([[0, 1], [0, 0]]) == 0
-assert obj.findChampion([[0, 0, 1], [1, 0, 1], [0, 0, 0]]) == 1
+assert obj.minOperations([1, 2, 7], [4, 5, 3]) == 1
+assert obj.minOperations([2, 3, 4, 5, 9], [8, 8, 4, 4, 4]) == 2
+assert obj.minOperations([1, 5, 4], [2, 5, 3]) == -1
