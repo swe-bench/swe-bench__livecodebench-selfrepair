c00b16eb6959f877993e7860ad1d52611c59ceb7
swe-bench <>
2024-06-12 16:11:54 -0400

    Task instance 3267

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
-    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+    def maximumLength(self, s: str) -> int:
         pass
 
 obj = Solution()
-assert obj.maximumSubarraySum([1, 2, 3, 4, 5, 6], 1) == 11
-assert obj.maximumSubarraySum([-1, 3, 2, 4, 5], 3) == 11
-assert obj.maximumSubarraySum([-1, -2, -3, -4], 2) == -6
+assert obj.maximumLength("aaaa") == 2
+assert obj.maximumLength("abcdef") == -1
+assert obj.maximumLength("abcaba") == 1
