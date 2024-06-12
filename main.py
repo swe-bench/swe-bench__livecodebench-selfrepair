96678d5e3bb0f8d884ae90d05e23f3eb84bb6bf9
swe-bench <>
2024-06-12 16:11:49 -0400

    Task instance 1873_A

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+def solution():
+    input_data = sys.stdin.read().strip()
+                    
+    # REPLACE WITH YOUR CODE HERE
+                    
+    # Remember, assign the output to output_data
+    output_data = None
+
+    print(output_data)
