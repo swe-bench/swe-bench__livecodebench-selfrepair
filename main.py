9608e49b84141ec66651dc265c7173c26adaac46
swe-bench <>
2024-06-12 16:11:50 -0400

    Task instance 2808

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import List
+
 class Solution:
-    def punishmentNumber(self, n: int) -> int:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.punishmentNumber(10) == 182
-assert obj.punishmentNumber(37) == 1478
+assert obj.paintWalls([1, 2, 3, 2], [1, 2, 3, 2]) == 3
+assert obj.paintWalls([2, 3, 4, 2], [1, 1, 1, 1]) == 4
