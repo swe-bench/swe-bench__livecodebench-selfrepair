f7b160021c43ec49dedbe6c1855e4fe9a84054e7
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3220

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.lexicographicallySmallestArray([1, 5, 3, 9, 8], 2) == [1, 3, 5, 8, 9]
-assert obj.lexicographicallySmallestArray([1, 7, 6, 18, 2, 1], 3) == [1, 6, 7, 18, 1, 2]
-assert obj.lexicographicallySmallestArray([1, 7, 28, 19, 10], 3) == [1, 7, 28, 19, 10]
+assert obj.countTestedDevices([1, 1, 2, 1, 3]) == 3
+assert obj.countTestedDevices([0, 1, 2]) == 2
