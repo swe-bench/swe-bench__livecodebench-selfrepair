13404f6578c0540cb25042808a5f44da5bfcc089
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3184

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 class Solution:
-    def findKOr(self, nums: List[int], k: int) -> int:
+    def maxBalancedSubsequenceSum(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.findKOr([7, 12, 9, 8, 9, 15], 4) == 9
-assert obj.findKOr([2, 12, 1, 11, 4, 5], 6) == 0
-assert obj.findKOr([10, 8, 5, 9, 11, 6, 8], 1) == 15
+assert obj.maxBalancedSubsequenceSum([3, 3, 5, 6]) == 14
+assert obj.maxBalancedSubsequenceSum([5, -1, -3, 8]) == 13
+assert obj.maxBalancedSubsequenceSum([-2, -1]) == -1
