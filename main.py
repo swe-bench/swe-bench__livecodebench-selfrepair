b17a191ab6796300063b83be440f4d45d9599099
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3192

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
-    def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
+    def maximumXorProduct(self, a: int, b: int, n: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minOperations([1, 2, 7], [4, 5, 3]) == 1
-assert obj.minOperations([2, 3, 4, 5, 9], [8, 8, 4, 4, 4]) == 2
-assert obj.minOperations([1, 5, 4], [2, 5, 3]) == -1
+assert obj.maximumXorProduct(12, 5, 4) == 98
+assert obj.maximumXorProduct(6, 7, 5) == 930
+assert obj.maximumXorProduct(1, 6, 3) == 12
