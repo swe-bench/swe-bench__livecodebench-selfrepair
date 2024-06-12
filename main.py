22e5fff1e4eecbf5fc00d6dec4b7f0a2008d7a49
swe-bench <>
2024-06-12 16:11:54 -0400

    Task instance 3239

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from typing import List
-
 class Solution:
-    def missingInteger(self, nums: List[int]) -> int:
+    def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
         pass
 
 obj = Solution()
-assert obj.missingInteger([1, 2, 3, 2, 5]) == 6
-assert obj.missingInteger([3, 4, 5, 1, 12, 14, 13]) == 15
+assert obj.minimumOperationsToMakeEqual(26, 1) == 3
+assert obj.minimumOperationsToMakeEqual(54, 2) == 4
+assert obj.minimumOperationsToMakeEqual(25, 30) == 5
