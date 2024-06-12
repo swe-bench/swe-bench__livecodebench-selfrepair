880611c1bac0f84ce40643c2568b8412da329f12
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3017

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
-    def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
+    def numberOfBeautifulIntegers(self, low: int, high: int, k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minAbsoluteDifference([4, 3, 2, 4], 2) == 0
-assert obj.minAbsoluteDifference([5, 3, 2, 10, 15], 1) == 1
-assert obj.minAbsoluteDifference([1, 2, 3, 4], 3) == 3
+assert obj.numberOfBeautifulIntegers(10, 20, 3) == 2
+assert obj.numberOfBeautifulIntegers(1, 10, 1) == 1
+assert obj.numberOfBeautifulIntegers(5, 5, 2) == 0
