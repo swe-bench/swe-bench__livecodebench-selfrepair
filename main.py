1b92a7171ad6f81a684ba04e6f0268f4ff44fdb6
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2870

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
+    def alternatingSubarray(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.maxNonDecreasingLength([2, 3, 1], [1, 2, 1]) == 2
-assert obj.maxNonDecreasingLength([1, 3, 2, 1], [2, 2, 3, 4]) == 4
-assert obj.maxNonDecreasingLength([1, 1], [2, 2]) == 2
+assert obj.alternatingSubarray([2, 3, 4, 3, 4]) == 4
+assert obj.alternatingSubarray([4, 5, 6]) == 2
