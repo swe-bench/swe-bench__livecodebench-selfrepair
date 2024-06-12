2fa46b9c15d73c20640d92b6daa7af24dcb7a310
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3200

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Solution:
-    def distributeCandies(self, n: int, limit: int) -> int:
+    def stringCount(self, n: int) -> int:
         pass
 
 obj = Solution()
-assert obj.distributeCandies(5, 2) == 3
-assert obj.distributeCandies(3, 3) == 10
+assert obj.stringCount(4) == 12
+assert obj.stringCount(10) == 83943898
