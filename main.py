5d3703e8a3b07ed4852120fa3f71260904079067
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3171

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def minGroupsForValidAssignment(self, nums: List[int]) -> int:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.minGroupsForValidAssignment([3, 2, 3, 2, 3]) == 2
-assert obj.minGroupsForValidAssignment([10, 10, 10, 3, 1, 1]) == 4
+assert obj.minSum([3, 2, 0, 1, 0], [6, 5, 0]) == 12
+assert obj.minSum([2, 0, 2, 0], [1, 4]) == -1
