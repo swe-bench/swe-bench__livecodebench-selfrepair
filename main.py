3ba5f4e71629a6657347bfd5d793ee25df70d0e2
swe-bench <>
2024-06-12 16:11:55 -0400

    Task instance 3317

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import List
+
 class Solution:
-    def countKeyChanges(self, s: str) -> int:
+    def maxPalindromesAfterOperations(self, words: List[str]) -> int:
         pass
 
 obj = Solution()
-assert obj.countKeyChanges("aAbBcC") == 2
-assert obj.countKeyChanges("AaAaAaaA") == 0
+assert obj.maxPalindromesAfterOperations(["abbb", "ba", "aa"]) == 3
+assert obj.maxPalindromesAfterOperations(["abc", "ab"]) == 2
+assert obj.maxPalindromesAfterOperations(["cd", "ef", "a"]) == 1
