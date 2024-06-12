70dafe0185499f140fa7f920683443b572d78247
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3210

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from typing import List
-
 class Solution:
-    def minimumCoins(self, prices: List[int]) -> int:
+    def beautifulSubstrings(self, s: str, k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minimumCoins([3, 1, 2]) == 4
-assert obj.minimumCoins([1, 10, 1, 1]) == 2
+assert obj.beautifulSubstrings("baeyh", 2) == 2
+assert obj.beautifulSubstrings("abba", 1) == 3
+assert obj.beautifulSubstrings("bcdf", 1) == 0
