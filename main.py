2cdd3cb085ae7b9d7b9740a88058de013675144f
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3207

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from typing import List
-
 class Solution:
-    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
         pass
 
 obj = Solution()
-assert obj.findIntersectionValues([4, 3, 2, 3, 1], [2, 2, 5, 2, 3, 6]) == [3, 4]
-assert obj.findIntersectionValues([3, 4, 2, 3], [1, 5]) == [0, 0]
+assert obj.findMinimumOperations("abc", "abb", "ab") == 2
+assert obj.findMinimumOperations("dac", "bac", "cac") == -1
