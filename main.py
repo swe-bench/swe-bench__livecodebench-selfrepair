e54d8a85a7d147800cfe31e66e3f9a2ebdb2ac73
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3209

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import List
+
 class Solution:
-    def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
+    def minimumCoins(self, prices: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.findMinimumOperations("abc", "abb", "ab") == 2
-assert obj.findMinimumOperations("dac", "bac", "cac") == -1
+assert obj.minimumCoins([3, 1, 2]) == 4
+assert obj.minimumCoins([1, 10, 1, 1]) == 2
