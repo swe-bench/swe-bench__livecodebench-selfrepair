ec2b7d3f6e82d80837c58e4e3b294568af0b19cb
swe-bench <>
2024-06-12 16:11:49 -0400

    Task instance 2728

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def countSeniors(self, details: List[str]) -> int:
+    def matrixSum(self, nums: List[List[int]]) -> int:
         pass
 
 obj = Solution()
-assert obj.countSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"]) == 2
-assert obj.countSeniors(["1313579440F2036", "2921522980M5644"]) == 0
+assert obj.matrixSum([[7, 2, 1], [6, 4, 2], [6, 5, 3], [3, 2, 1]]) == 15
+assert obj.matrixSum([[1]]) == 1
