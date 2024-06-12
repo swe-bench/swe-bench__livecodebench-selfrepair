738b9c910bd354669a9336e13b4d3e247bc1f8d2
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2954

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import List
 
 class Solution:
-    def countPairs(self, coordinates: List[List[int]], k: int) -> int:
+    def maxSum(self, nums: List[int], m: int, k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.countPairs([[1, 2], [4, 2], [1, 3], [5, 2]], 5) == 2
-assert obj.countPairs([[1, 3], [1, 3], [1, 3], [1, 3], [1, 3]], 0) == 10
+assert obj.maxSum([2, 6, 7, 3, 1, 7], 3, 4) == 18
+assert obj.maxSum([5, 9, 9, 2, 4, 5, 4], 1, 3) == 23
+assert obj.maxSum([1, 2, 1, 2, 1, 2, 1], 3, 3) == 0
