9e1e00c447dbcd3043294af6ae5a66084a106eee
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3226

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+    def numberGame(self, nums: List[int]) -> List[int]:
         pass
 
 obj = Solution()
-assert obj.maxSubarrayLength([1, 2, 3, 1, 2, 3, 1, 2], 2) == 6
-assert obj.maxSubarrayLength([1, 2, 1, 2, 1, 2, 1, 2], 1) == 2
-assert obj.maxSubarrayLength([5, 5, 5, 5, 5, 5, 5], 4) == 4
+assert obj.numberGame([5, 4, 2, 3]) == [3, 2, 5, 4]
+assert obj.numberGame([2, 5]) == [5, 2]
