b84ddca6f1215a88a0ee8b2e11a632077a11a2ae
swe-bench <>
2024-06-12 16:11:54 -0400

    Task instance 3297

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
-    def earliestSecondToMarkIndices(self, nums: List[int], changeIndices: List[int]) -> int:
+    def minimumTimeToInitialState(self, word: str, k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.earliestSecondToMarkIndices([2, 2, 0], [2, 2, 2, 2, 3, 2, 2, 1]) == 8
-assert obj.earliestSecondToMarkIndices([1, 3], [1, 1, 1, 2, 1, 1, 1]) == 6
-assert obj.earliestSecondToMarkIndices([0, 1], [2, 2, 2]) == -1
+assert obj.minimumTimeToInitialState("abacaba", 3) == 2
+assert obj.minimumTimeToInitialState("abacaba", 4) == 1
+assert obj.minimumTimeToInitialState("abcbabcd", 2) == 4
