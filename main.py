89d4c9f8fcef99dfb73d4093ee81452137b2fd08
swe-bench <>
2024-06-12 16:11:50 -0400

    Task instance 2849

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def specialPerm(self, nums: List[int]) -> int:
+    def sumImbalanceNumbers(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.specialPerm([2, 3, 6]) == 2
-assert obj.specialPerm([1, 4, 3]) == 2
+assert obj.sumImbalanceNumbers([2, 3, 1, 4]) == 3
+assert obj.sumImbalanceNumbers([1, 3, 3, 3, 5]) == 8
