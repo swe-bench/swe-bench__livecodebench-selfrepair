b7283605d9a1a2d33a064a56481db8eb5ef417ae
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 2955

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from typing import List
-
 class Solution:
-    def maxSum(self, nums: List[int], m: int, k: int) -> int:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
         pass
 
 obj = Solution()
-assert obj.maxSum([2, 6, 7, 3, 1, 7], 3, 4) == 18
-assert obj.maxSum([5, 9, 9, 2, 4, 5, 4], 1, 3) == 23
-assert obj.maxSum([1, 2, 1, 2, 1, 2, 1], 3, 3) == 0
+assert obj.accountBalanceAfterPurchase(9) == 90
+assert obj.accountBalanceAfterPurchase(15) == 80
