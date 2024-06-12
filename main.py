d743671e3ffc396d619465d1291e796790bf0e2a
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3104

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
+    def countWays(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.minOperations([2, 3, 3, 2, 2, 4, 2, 3, 4]) == 4
-assert obj.minOperations([2, 1, 2, 2, 3, 3]) == -1
+assert obj.countWays([1, 1]) == 2
+assert obj.countWays([6, 0, 3, 3, 6, 7, 2, 7]) == 3
