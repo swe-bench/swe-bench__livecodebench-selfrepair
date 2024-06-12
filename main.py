13d544133b02b296e1d5cf1eadea14ad799baaff
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3194

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 class Solution:
-    def maximumStrongPairXor(self, nums: List[int]) -> int:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         pass
 
 obj = Solution()
-assert obj.maximumStrongPairXor([1, 2, 3, 4, 5]) == 7
-assert obj.maximumStrongPairXor([10, 100]) == 0
-assert obj.maximumStrongPairXor([5, 6, 25, 30]) == 7
+assert obj.findWordsContaining(["leet", "code"], "e") == [0, 1]
+assert obj.findWordsContaining(["abc", "bcd", "aaaa", "cbc"], "a") == [0, 2]
+assert obj.findWordsContaining(["abc", "bcd", "aaaa", "cbc"], "z") == []
