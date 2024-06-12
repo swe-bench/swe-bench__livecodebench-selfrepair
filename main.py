51d440e6e9d6795cc8cf9cfcaa126f29395af90b
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2867

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+    def numberOfGoodSubarraySplits(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.longestAlternatingSubarray([3, 2, 5, 4], 5) == 3
-assert obj.longestAlternatingSubarray([1, 2], 2) == 1
-assert obj.longestAlternatingSubarray([2, 3, 4, 5], 4) == 3
+assert obj.numberOfGoodSubarraySplits([0, 1, 0, 0, 1]) == 3
+assert obj.numberOfGoodSubarraySplits([0, 1, 0]) == 1
