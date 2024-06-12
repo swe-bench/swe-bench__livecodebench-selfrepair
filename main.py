70f08294d74bf6101b61449ed35bd6c34f9b411c
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3212

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 class Solution:
-    def findMaximumLength(self, nums: List[int]) -> int:
+    def numberOfGoodPartitions(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.findMaximumLength([5, 2, 2]) == 1
-assert obj.findMaximumLength([1, 2, 3, 4]) == 4
-assert obj.findMaximumLength([4, 3, 2, 6]) == 3
+assert obj.numberOfGoodPartitions([1, 2, 3, 4]) == 8
+assert obj.numberOfGoodPartitions([1, 1, 1, 1]) == 1
+assert obj.numberOfGoodPartitions([1, 2, 1, 3]) == 2
