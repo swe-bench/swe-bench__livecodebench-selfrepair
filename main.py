c9d5e10b158bd40ab259d484384a8afb4604c3c1
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3080

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import List
+
 class Solution:
-    def maximumOddBinaryNumber(self, s: str) -> str:
+    def maxSubarrays(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.maximumOddBinaryNumber("010") == "001"
-assert obj.maximumOddBinaryNumber("0101") == "1001"
+assert obj.maxSubarrays([1, 0, 2, 0, 1, 2]) == 3
+assert obj.maxSubarrays([5, 7, 1, 3]) == 1
