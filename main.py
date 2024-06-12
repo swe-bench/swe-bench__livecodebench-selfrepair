b0aa337f6c65430e24f9b06c8eb9c5c5bc613c61
swe-bench <>
2024-06-12 16:11:53 -0400

    Task instance 3188

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def maxBalancedSubsequenceSum(self, nums: List[int]) -> int:
+    def findChampion(self, grid: List[List[int]]) -> int:
         pass
 
 obj = Solution()
-assert obj.maxBalancedSubsequenceSum([3, 3, 5, 6]) == 14
-assert obj.maxBalancedSubsequenceSum([5, -1, -3, 8]) == 13
-assert obj.maxBalancedSubsequenceSum([-2, -1]) == -1
+assert obj.findChampion([[0, 1], [0, 0]]) == 0
+assert obj.findChampion([[0, 0, 1], [1, 0, 1], [0, 0, 0]]) == 1
