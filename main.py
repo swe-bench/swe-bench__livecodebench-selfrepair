e652e4ca6ca196cd427c743051e70471f9bbc90d
swe-bench <>
2024-06-12 16:11:50 -0400

    Task instance 2839

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import List
+
 class Solution:
-    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+    def maximumSumQueries(self, nums1: List[int], nums2: List[int], queries: List[List[int]]) -> List[int]:
         pass
 
 obj = Solution()
-assert obj.makeTheIntegerZero(3, -2) == 3
-assert obj.makeTheIntegerZero(5, 7) == -1
+assert obj.maximumSumQueries([4, 3, 1, 2], [2, 4, 9, 5], [[4, 1], [1, 3], [2, 5]]) == [6, 10, 7]
+assert obj.maximumSumQueries([3, 2, 5], [2, 3, 4], [[4, 4], [3, 2], [1, 1]]) == [9, 9, 9]
+assert obj.maximumSumQueries([2, 1], [2, 3], [[3, 3]]) == [-1]
