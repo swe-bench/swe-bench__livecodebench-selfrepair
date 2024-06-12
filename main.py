6d0207e73427e0617989180b8623d0ef1b7c1b9e
swe-bench <>
2024-06-12 16:11:52 -0400

    Task instance 3025

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import List
+
 class Solution:
-    def numberOfWays(self, s: str, t: str, k: int) -> int:
+    def minOperations(self, nums: List[int], target: int) -> int:
         pass
 
 obj = Solution()
-assert obj.numberOfWays("abcd", "cdab", 2) == 2
-assert obj.numberOfWays("ababab", "ababab", 1) == 2
+assert obj.minOperations([1, 2, 8], 7) == 1
+assert obj.minOperations([1, 32, 1, 2], 12) == 2
+assert obj.minOperations([1, 32, 1], 35) == -1
