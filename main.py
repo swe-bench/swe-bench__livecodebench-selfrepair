f23cea6e85911632f5bea044eccd998f7677e2ff
swe-bench <>
2024-06-12 16:11:50 -0400

    Task instance 2825

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Solution:
-    def isFascinating(self, n: int) -> bool:
+    def minimizedStringLength(self, s: str) -> int:
         pass
 
 obj = Solution()
-assert obj.isFascinating(192) == True
-assert obj.isFascinating(100) == False
+assert obj.minimizedStringLength("aaabc") == 3
+assert obj.minimizedStringLength("cbbd") == 3
+assert obj.minimizedStringLength("dddaaa") == 2
