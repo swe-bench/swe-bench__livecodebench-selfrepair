becd68468e79c9f50aa82effaa8dfe606edae75a
swe-bench <>
2024-06-12 16:11:50 -0400

    Task instance 2844

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Solution:
-    def maximumSumQueries(self, nums1: List[int], nums2: List[int], queries: List[List[int]]) -> List[int]:
+    def sumOfSquares(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.maximumSumQueries([4, 3, 1, 2], [2, 4, 9, 5], [[4, 1], [1, 3], [2, 5]]) == [6, 10, 7]
-assert obj.maximumSumQueries([3, 2, 5], [2, 3, 4], [[4, 4], [3, 2], [1, 1]]) == [9, 9, 9]
-assert obj.maximumSumQueries([2, 1], [2, 3], [[3, 3]]) == [-1]
+assert obj.sumOfSquares([1, 2, 3, 4]) == 21
+assert obj.sumOfSquares([2, 7, 1, 19, 18, 3]) == 63
