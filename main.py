e882f4adcdf920eb9a45b7136e7cc6a4a178bec9
swe-bench <>
2024-06-12 16:11:51 -0400

    Task instance 2888

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import List
+
 class Solution:
-    def sortVowels(self, s: str) -> str:
+    def minimumIndex(self, nums: List[int]) -> int:
         pass
 
 obj = Solution()
-assert obj.sortVowels("lEetcOde") == "lEOtcede"
-assert obj.sortVowels("lYmpH") == "lYmpH"
+assert obj.minimumIndex([1, 2, 2, 2]) == 2
+assert obj.minimumIndex([2, 1, 3, 1, 1, 1, 7, 1, 2, 1]) == 4
+assert obj.minimumIndex([3, 3, 3, 3, 7, 2, 2]) == -1
