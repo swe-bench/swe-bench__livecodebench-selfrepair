7f393c374f737f3f53caf1fde113d164392bc538
swe-bench <>
2024-06-12 16:11:54 -0400

    Task instance 3233

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
-    def minimumAddedCoins(self, coins: List[int], target: int) -> int:
+    def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
         pass
 
 obj = Solution()
-assert obj.minimumAddedCoins([1, 4, 10], 19) == 2
-assert obj.minimumAddedCoins([1, 4, 10, 5, 7, 19], 19) == 1
-assert obj.minimumAddedCoins([1, 1, 1], 20) == 3
+assert obj.maxPartitionsAfterOperations("accca", 2) == 3
+assert obj.maxPartitionsAfterOperations("aabaab", 3) == 1
+assert obj.maxPartitionsAfterOperations("xxyz", 1) == 4
